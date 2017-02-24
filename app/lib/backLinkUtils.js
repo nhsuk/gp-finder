e80be22d2fb9b70c51978be9c28fea6cacd8d159
Eva Barabas <b_seven_e@yahoo.co.uk>
2017-02-24 08:52:02 +0000

    :relaxed: make code more readable, use specific tag

--- app/lib/backLinkUtils.js
+++ app/lib/backLinkUtils.js
@@ -1,13 +1,8 @@
-function url(req) {
-  // eslint-disable-next-line no-script-url
-  const link = req.get('referer') || 'javascript:history.back();';
-  return link;
-}
-
 function backLinkUtils(req) {
   return {
     text: 'Back',
-    url: url(req),
+    // eslint-disable-next-line no-script-url
+    url: req.get('referer') || 'javascript:history.back();',
   };
 }
 
