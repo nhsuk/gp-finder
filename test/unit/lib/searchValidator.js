f6f2a3d1832c890c351f05e2340869d5c1a49d8f
Eva Barabas <b_seven_e@yahoo.co.uk>
2017-03-01 12:51:33 +0000

    :leaves: refactor for clarity

--- test/unit/lib/searchValidator.js
+++ test/unit/lib/searchValidator.js
@@ -1,5 +1,5 @@
 const chai = require('chai');
-const validateSearch = require('../../../app/lib/searchValidator');
+const searchValidator = require('../../../app/lib/searchValidator');
 const messages = require('../../../app/lib/messages');
 
 const expect = chai.expect;
@@ -7,9 +7,29 @@ const expect = chai.expect;
 describe('Search validation', () => {
   describe('error handling', () => {
     it('should return an errorMessage when no search is provided', () => {
+      const emptySearch = null;
+
+      const result = searchValidator.validateSearch(emptySearch);
+
+      expect(result.errorMessage)
+        .to.be
+        .equal(messages.emptySearchMessage());
+    });
+
+    it('should return an errorMessage when search is an empty string', () => {
       const emptySearch = '';
 
-      const result = validateSearch(emptySearch);
+      const result = searchValidator.validateSearch(emptySearch);
+
+      expect(result.errorMessage)
+        .to.be
+        .equal(messages.emptySearchMessage());
+    });
+
+    it('should return an errorMessage when search only has spaces', () => {
+      const emptySearch = '   ';
+
+      const result = searchValidator.validateSearch(emptySearch);
 
       expect(result.errorMessage)
         .to.be
@@ -19,7 +39,7 @@ describe('Search validation', () => {
     it('should return an object with expected properties', () => {
       const emptySearch = '';
 
-      const result = validateSearch(emptySearch);
+      const result = searchValidator.validateSearch(emptySearch);
 
       expect(result).to.be.an('object');
       expect(result).to.have.property('input');
@@ -31,7 +51,7 @@ describe('Search validation', () => {
     describe('for some search term', () => {
       it('should return the input', () => {
         const someSearch = 'ab';
-        const result = validateSearch(someSearch);
+        const result = searchValidator.validateSearch(someSearch);
 
         expect(result.input).to.be.equal(someSearch);
         // eslint-disable-next-line no-unused-expressions
@@ -41,7 +61,7 @@ describe('Search validation', () => {
       it('should return the trimmed input', () => {
         const someSearch = ' ab ';
         const trimmedSearch = 'ab';
-        const result = validateSearch(someSearch);
+        const result = searchValidator.validateSearch(someSearch);
 
         expect(result.input).to.be.equal(trimmedSearch);
         // eslint-disable-next-line no-unused-expressions
