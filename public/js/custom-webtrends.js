a6e829e4e10b394ff9af1f52e0e5a80bfdca073c
Eva Barabas <b_seven_e@yahoo.co.uk>
2017-04-10 14:13:34 +0100

    :unamused: remove extra code

--- public/js/custom-webtrends.js
+++ public/js/custom-webtrends.js
@@ -1,11 +1,7 @@
 jQuery(function($) {
   $(document).ready(function () {
     $('.not-registered').on('toggle', function () {
-      window.Webtrends.multiTrack({argsa: ['DCSext.GPAppointment', 'Not_Registered', 'WT.dl', '121']});
-      // Webtrends.multiTrack({
-      //   element: $(this),
-      //   argsa: ['DCSext.GPAppointment', 'Not_Registered', 'WT.dl', '121']
-      // });
+      Webtrends.multiTrack({argsa: ['DCSext.GPAppointment', 'Not_Registered', 'WT.dl', '121']});
     });
   });
 });
