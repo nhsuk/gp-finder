3fc977926a8260ab82c9db7e7e811fd11f2642d0
ajthomascouk <alex.thomas@nhs.net>
2017-04-12 12:47:49 +0100

    :grapes: add WT tracking to results with index

--- public/js/custom-webtrends.js
+++ public/js/custom-webtrends.js
@@ -3,5 +3,8 @@ jQuery(function($) {
     $('.not-registered').on('toggle', function () {
       Webtrends.multiTrack({argsa: ['DCSext.GPAppointment', 'Not_Registered', 'WT.dl', '121']});
     });
+    $('.results__item h3 a').on('touchstart click', function () {
+      Webtrends.multiTrack({argsa: ['DCSext.GPAppClick', $(this).text(), 'DCSext.GPAppRank', $(this).parents(".results__item").data('index'), 'WT.dl', '121']});
+    });
   });
 });
