92583afa55dacd050b1464d277e2814f7e030b1b
Eva Barabas <b_seven_e@yahoo.co.uk>
2017-04-27 07:32:24 +0100

    :book: refactor based on previous feedback on different PR

--- test/unit/lib/utils/gpDataMapper.js
+++ test/unit/lib/utils/gpDataMapper.js
@@ -4,50 +4,26 @@ const gpDataMapper = require('../../../../app/lib/utils/gpDataMapper');
 const expect = chai.expect;
 
 describe('GP Surgery Data Mapper', () => {
-<<<<<<< HEAD
-  it('for a GP Surgery that has appointments POMI data and a url will return the url', () => {
-    const gpData = { onlineServices: { appointments: { supplier: 'EMIS', url: 'http://example.com' } } };
-    const result = gpDataMapper(gpData);
-
-    expect(result).to.be.equal('http://example.com');
-  });
-
-  it('for a GP Surgery that has no appointments POMI data will return undefined', () => {
-    const gpData = { onlineServices: { repeatPrescription: {} } };
-    const result = gpDataMapper(gpData);
-
-    expect(result).to.be.equal(undefined);
-  });
-
-  it('for a GP Surgery that has no online services  data will return undefined', () => {
-    const gpData = {};
-    const result = gpDataMapper(gpData);
-
-    expect(result).to.be.equal(undefined);
-=======
   describe('booking url', () => {
     it('for a GP Surgery that has POMI data and a url will return the url', () => {
       const gpData = { bookingSystem: { supplier: 'EMIS', bookOnlineLink: 'http://example.com' } };
-      const expectedResult = 'http://example.com';
       const result = gpDataMapper.getBookOnlineLink(gpData);
 
-      expect(result).to.be.equal(expectedResult);
+      expect(result).to.be.equal('http://example.com');
     });
 
     it('for a GP Surgery that has POMI data but no url will return undefined', () => {
       const gpData = { bookingSystem: { supplier: 'EMIS (I)' } };
-      const expectedResult = undefined;
       const result = gpDataMapper.getBookOnlineLink(gpData);
 
-      expect(result).to.be.equal(expectedResult);
+      expect(result).to.be.equal(undefined);
     });
 
     it('for a GP Surgery that has no POMI data will return undefined', () => {
       const gpData = {};
-      const expectedResult = undefined;
       const result = gpDataMapper.getBookOnlineLink(gpData);
 
-      expect(result).to.be.equal(expectedResult);
+      expect(result).to.be.equal(undefined);
     });
   });
 
@@ -56,29 +32,25 @@ describe('GP Surgery Data Mapper', () => {
     it('for a GP Surgery that has no doctors with the name matching the search will not return any doctors', () => {
       const gpData = { doctors: ['Dr. Farooq', 'Dr. Carter'] };
       const searchStr = 'Roberts';
-      const expectedResult = [];
       const result = gpDataMapper.getFilteredGps(gpData, searchStr);
 
-      expect(result).to.be.eql(expectedResult);
+      expect(result).to.be.eql([]);
     });
 
     it('for a GP Surgery that has no doctors  will not return any doctors', () => {
       const gpData = {};
       const searchStr = 'Roberts';
-      const expectedResult = undefined;
       const result = gpDataMapper.getFilteredGps(gpData, searchStr);
 
-      expect(result).to.be.eql(expectedResult);
+      expect(result).to.be.eql(undefined);
     });
 
     it('for a GP Surgery that has doctors with the name matching the search will return those doctors', () => {
       const gpData = { doctors: ['Dr. A Farooq', 'Dr. Carter', 'Dr. B Farooq'] };
       const searchStr = 'Farooq';
-      const expectedResult = ['Dr. A Farooq', 'Dr. B Farooq'];
       const result = gpDataMapper.getFilteredGps(gpData, searchStr);
 
-      expect(result).to.be.eql(expectedResult);
+      expect(result).to.be.eql(['Dr. A Farooq', 'Dr. B Farooq']);
     });
->>>>>>> 25a6678... :scissors: only return searched for doctors
   });
 });
