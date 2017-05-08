a0cd32351ba5bdd068ad09e0c9c5281353ed3896
ajthomascouk <alex.thomas@nhs.net>
2017-05-08 13:03:06 +0100

    :full_moon: multi word highlight

--- test/unit/lib/messages.js
+++ test/unit/lib/messages.js
@@ -6,8 +6,8 @@ const expect = chai.expect;
 describe('messages', () => {
   it('should have an error message for when nothing has been entered to search with', () => {
     const message = messages.emptySearchMessage();
-
-    expect(message).to.equal('Search using the name of your surgery or GP.');
+  
+    expect(message).to.equal('Search using the name of your surgery or GP');
   });
 
   it('should have an error message for technical problems', () => {
