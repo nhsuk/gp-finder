c58840c02b4fa32cb6bc0d4389eebc15e5dc5497
ajthomascouk <alex.thomas@nhs.net>
2017-05-08 13:03:06 +0100

    :full_moon: multi word highlight

--- public/js/term-highlight.js
+++ public/js/term-highlight.js
@@ -1,15 +1,22 @@
 jQuery(function($) {
   $(document).ready(function () {
-    var searchTerm = $('h1.sr-only').data('term');
-    $('.results__item').each(function(){
-      var src_str = $(this).html();
-      searchTerm = searchTerm.replace(/(\s+)/,"(<[^>]+>)*$1(<[^>]+>)*");
-      var pattern = new RegExp("("+searchTerm+")", "gi");
+    if ($('h1.sr-only').data('term')){
+      //var searchTerm = $('h1.sr-only').data('term');
+      var terms = $('h1.sr-only').data('term').split(/\W+/);
+      $('.results__item').each(function(){
+        var src_str = $(this).html();
+        var results_item = $(this)
+        $.each(terms, function(_, term){
+          term = term.replace(/(\s+)/,"(<[^>]+>)*$1(<[^>]+>)*");
+          var pattern = new RegExp("("+term+")", "gi");
 
-      src_str = src_str.replace(pattern, '<span class="highlight">$1</span>');
-      src_str = src_str.replace(/(<span class="highlight">[^<>]*)((<[^>]+>)+)([^<>]*<\/span>)/,'$1</span>$2<span class="highlight">$4');
+          src_str = src_str.replace(pattern, '<span class="highlight">$1</span>');
+          src_str = src_str.replace(/(<span class="highlight">[^<>]*)((<[^>]+>)+)([^<>]*<\/span>)/,'$1</span>$2<span class="highlight">$4');
 
-      $(this).html(src_str);
-    });
+          $(results_item).html(src_str);
+
+        });
+      });
+    }
   });
 });
