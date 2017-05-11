3792ece8ca03c2d23f1ea928ede78d4c15dc1a88
ajthomascouk <alex.thomas@nhs.net>
2017-05-11 12:21:42 +0100

    :bookmark: include mark.js to perform highlighting.

--- public/js/term-highlight.js
+++ public/js/term-highlight.js
@@ -1,25 +1,18 @@
 jQuery(function($) {
   $(document).ready(function () {
-    if ($('h1.sr-only').data('term')){
-      var searchTerm = $('h1.sr-only').data('term');
-      var terms = searchTerm.split(/\W+/);
-      if ($.inArray( searchTerm, terms ) === -1){
-        terms.push(searchTerm);
-      }
-      $('.results__item').each(function(){
-        var src_str = $(this).html();
-        var results_item = $(this)
-        $.each(terms, function(_, term){
-          term = term.replace(/(\s+)/,"(<[^>]+>)*$1(<[^>]+>)*");
-          var pattern = new RegExp("("+term+")", "gi");
-
-          src_str = src_str.replace(pattern, '<span class="highlight">$1</span>');
-          src_str = src_str.replace(/(<span class="highlight">[^<>]*)((<[^>]+>)+)([^<>]*<\/span>)/,'$1</span>$2<span class="highlight">$4');
-
-          $(results_item).html(src_str);
-
-        });
-      });
-    }
+    var searchTerm = $('h1.sr-only').data('term');
+        // remove speach mark
+        searchTerm = searchTerm.replace(/["]/g,'');
+        // remove special characters
+        searchTerm = searchTerm.replace(/[;]/g,'');
+    console.log(searchTerm);
+    $('.results').mark(searchTerm, {
+        "wildcards": "withSpaces",
+        "ignoreJoiners": true,
+        "acrossElements": true,
+        "separateWordSearch": true,
+        "element": "em",
+        "className": "highlight"
+    });
   });
 });
