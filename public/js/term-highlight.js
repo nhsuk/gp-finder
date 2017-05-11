99a191cfa540fadf824296080fa08f002ed5ca0c
ajthomascouk <alex.thomas@nhs.net>
2017-05-11 15:21:00 +0100

    :cat2: improved search highlighting

--- public/js/term-highlight.js
+++ public/js/term-highlight.js
@@ -1,18 +1,32 @@
 jQuery(function($) {
   $(document).ready(function () {
-    var searchTerm = $('h1.sr-only').data('term');
+    var searchTerm = $('h1.sr-only').data('term').toLowerCase();
         // remove speach mark
-        searchTerm = searchTerm.replace(/["]/g,'');
+        searchTerm = searchTerm.replace(/[".]/g,'');
         // remove special characters
-        searchTerm = searchTerm.replace(/[;]/g,'');
-    console.log(searchTerm);
-    $('.results').mark(searchTerm, {
+        searchTerm = searchTerm.replace(/[;]/g,' ');
+        // Change Dr to Doctor
+        searchTerm = searchTerm.replace('doc','dr');
+
+    var terms = searchTerm.split(/\W+/);
+
+    for (var i = 0, len = terms.length; i < len-1; i++) {
+      terms.push(terms[i]+' '+terms[i+1]);
+    }
+    if ($.inArray( searchTerm, terms ) === -1){
+      terms.push(searchTerm);
+    }
+
+    terms.forEach(function(term, index){
+      $('.results').mark(term, {
         "wildcards": "withSpaces",
         "ignoreJoiners": true,
         "acrossElements": true,
-        "separateWordSearch": true,
+        "separateWordSearch": false,
         "element": "em",
-        "className": "highlight"
+        "className": "highlight",
+        "accuracy": "exactly"
+      });
     });
   });
 });
