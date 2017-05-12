fb5c9bd2f6e59b539a890c3a0b2d94ae7967aabc
Eva Barabas <b_seven_e@yahoo.co.uk>
2017-05-12 09:14:51 +0100

    :mask: sanitize dr's names, tests, tests for dr

--- test/unit/lib/utils/gpDataMapper.js
+++ test/unit/lib/utils/gpDataMapper.js
@@ -28,7 +28,7 @@ describe('GP Surgery Data Mapper', () => {
   });
 
   // Array comparison: https://medium.com/@victorleungtw/testing-with-mocha-array-comparison-e9a45b57df27
-  describe('filteted doctors', () => {
+  describe('filtered doctors', () => {
     it('for a GP Surgery that has no doctors with the name matching the search will not return any doctors', () => {
       const gpData = { doctors: ['Dr. Farooq', 'Dr. Carter'] };
       const searchStr = 'Roberts';
@@ -53,12 +53,70 @@ describe('GP Surgery Data Mapper', () => {
       expect(result).to.be.eql(['Dr. A Farooq', 'Dr. B Farooq']);
     });
 
-    it('for a GP Surgery that has doctors with the some of the name matching the search will return those doctors', () => {
+    it('for a GP Surgery that has doctors with some of the name matching the search will return those doctors', () => {
       const gpData = { doctors: ['Dr. Elizabeth Beech', 'Dr. Elizabeth Ash', 'Dr. Elizabeth', 'Dr. B Beech'] };
       const searchStr = 'Elizabeth Beech';
       const result = gpDataMapper.getFilteredGps(gpData, searchStr);
 
       expect(result).to.be.eql(['Dr. Elizabeth Beech', 'Dr. Elizabeth Ash', 'Dr. Elizabeth', 'Dr. B Beech']);
+    });
+
+    it('for a GP Surgery that has Dr/Dr. in the title but not related to the search it should be ignored', () => {
+      const gpData = { doctors: ['Dr. Elizabeth Beech', 'Dr. Elizabeth Ash', 'Dr. Elizabeth', 'Dr. B Beech'] };
+      const searchStr = 'Dr Beech';
+      const result = gpDataMapper.getFilteredGps(gpData, searchStr);
+
+      expect(result).to.be.eql(['Dr. Elizabeth Beech', 'Dr. B Beech']);
+    });
+  });
+
+  describe('mapped title for doctors', () => {
+    it('for doctors that have variations of Doctor/Other/M* in their title should be changed to Dr', () => {
+      const doctors = ['Doctor Elizabeth Beech', 'Other Elizabeth Ash', 'Mrs Elizabeth', 'Dr. B Beech', 'Dr Beech'];
+      const result = gpDataMapper.mappedTitleForGps(doctors);
+
+      expect(result).to.be.eql(['Dr Elizabeth Beech', 'Dr Elizabeth Ash', 'Dr Elizabeth', 'Dr B Beech', 'Dr Beech']);
+    });
+
+    it('for doctors that have variations of Prof/Proffessor in their title should be changed to Professor and have a GP at the end', () => {
+      const doctors = ['Prof Elizabeth Beech', 'Proffessor Elizabeth Ash', 'Prof. Elizabeth', 'Pro B Beech'];
+      const result = gpDataMapper.mappedTitleForGps(doctors);
+
+      expect(result).to.be.eql(['Professor Elizabeth Beech (GP)', 'Professor Elizabeth Ash (GP)', 'Professor Elizabeth (GP)', 'Professor B Beech (GP)']);
+    });
+
+    it('for doctors that have variations of drs in their title should be changed to Drs', () => {
+      const doctors = ['drs Smith', 'DRS Smith'];
+      const result = gpDataMapper.mappedTitleForGps(doctors);
+
+      expect(result).to.be.eql(['Drs Smith', 'Drs Smith']);
+    });
+
+    it('for doctors that have variations of General Practitioner|Medi-Access Surgery in their title should be removed', () => {
+      const doctors = ['General Practitioner Dr Smith', 'Medi-Access Surgery Dr Smith'];
+      const result = gpDataMapper.mappedTitleForGps(doctors);
+
+      expect(result).to.be.eql(['Dr Smith', 'Dr Smith']);
+    });
+
+    it('for doctors that have variations of Locum/The in their title should be ignored', () => {
+      const doctors = ['Locum Smith', 'The Greatest'];
+      const result = gpDataMapper.mappedTitleForGps(doctors);
+
+      expect(result).to.be.eql(['Locum Smith', 'The Greatest']);
+    });
+
+    it('for doctors that have nothing in their title should be changed to Dr', () => {
+      const doctors = ['Smith', 'A Farooq'];
+      const result = gpDataMapper.mappedTitleForGps(doctors);
+
+      expect(result).to.be.eql(['Dr Smith', 'Dr A Farooq']);
+    });
+    it('for doctors that have ./space in their title should be changed to Dr', () => {
+      const doctors = [' Smith', '.A Farooq'];
+      const result = gpDataMapper.mappedTitleForGps(doctors);
+
+      expect(result).to.be.eql(['Dr Smith', 'Dr A Farooq']);
     });
   });
 });
