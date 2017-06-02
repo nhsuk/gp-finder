dc5ad6194f048f05e9a72b4b54f4b5701486707f
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-06-02 10:21:33 +0100

    :eyes: Finesse the searching of stop words.

--- test/unit/lib/esQueryBuilder.js
+++ test/unit/lib/esQueryBuilder.js
@@ -10,13 +10,6 @@ describe('esQueryBuilder', () => {
     expect(query).to.be.an('object');
   });
 
-  it('should return the search term as q param', () => {
-    const searchTerm = 'search for this';
-    const query = esQueryBuilder.build(searchTerm);
-
-    expect(query.body.query.multi_match.query).to.be.equal(searchTerm);
-  });
-
   it('should return the size as 30', () => {
     const query = esQueryBuilder.build();
 
