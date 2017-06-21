9d80321bcab5981cb9f0bc89cd839d615a7ffbf4
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-06-21 08:10:09 +0100

    :postbox: Add support for outcodes

--- app/middleware/postcodeLookup.js
+++ app/middleware/postcodeLookup.js
@@ -3,19 +3,17 @@ const PostcodesIO = require('postcodesio-client');
 const postcodes = new PostcodesIO();
 
 function lookupPostcode(req, res, next) {
+  const outcodeRegex = /^[A-Z]{1,2}[0-9][0-9A-Z]?$/gi;
   const postcodeRegex = /^[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}$/gi;
   const search = res.locals.search;
 
-  if (search.match(postcodeRegex)) {
+  if (search.match(outcodeRegex)) {
+    postcodes.outcode(search, (err, postcode) => {
+      res.locals.location = { lat: postcode.latitude, lon: postcode.longitude };
+      next();
+    });
+  } else if (search.match(postcodeRegex)) {
     postcodes.lookup(search, (err, postcode) => {
-      // {
-      //   "postcode": "EC1V 9LB",
-      //   "admin_district": "Islington",
-      //   "longitude": -0.091247681768113,
-      //   "latitude": 51.5278436902703,
-      //   "region": "London",
-      //   ...
-      // }
       res.locals.location = { lat: postcode.latitude, lon: postcode.longitude };
       next();
     });
