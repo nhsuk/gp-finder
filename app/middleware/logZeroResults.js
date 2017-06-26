cb41e2878b0c0f5ef435652717504cfabe382139
Eva Barabas <b_seven_e@yahoo.co.uk>
2017-06-26 16:58:26 +0100

    :v: account for different error messaging for no results

--- app/middleware/logZeroResults.js
+++ app/middleware/logZeroResults.js
@@ -1,7 +1,7 @@
 const log = require('../lib/logger');
 
 function logZeroResults(req, res, next) {
-  const search = res.locals.search;
+  const search = res.locals.processedSearch;
 
   if (res.locals.gps.length === 0) {
     log.warn({ search }, `No results were found for ${search}`);
