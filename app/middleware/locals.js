5b1f88ae1a2d87ed0192c12b0f8fccc393a654e5
steven hunt <st3v3n.hunt@gmail.com>
2017-07-05 17:12:35 +0100

    :lock: Update yarn.lock on Greenkeeper PRs

--- app/middleware/locals.js
+++ app/middleware/locals.js
@@ -1,11 +1,11 @@
 module.exports = config =>
-(req, res, next) => {
+  (req, res, next) => {
   /* eslint-disable no-param-reassign */
-  res.locals.GOOGLE_ANALYTICS_TRACKING_ID = config.googleAnalyticsId;
-  res.locals.WEBTRENDS_ANALYTICS_TRACKING_ID = config.webtrendsId;
-  res.locals.HOTJAR_ANALYTICS_TRACKING_ID = config.hotjarId;
-  res.locals.SITE_ROOT = req.app.locals.SITE_ROOT;
-  /* eslint-enable no-param-reassign */
+    res.locals.GOOGLE_ANALYTICS_TRACKING_ID = config.googleAnalyticsId;
+    res.locals.WEBTRENDS_ANALYTICS_TRACKING_ID = config.webtrendsId;
+    res.locals.HOTJAR_ANALYTICS_TRACKING_ID = config.hotjarId;
+    res.locals.SITE_ROOT = req.app.locals.SITE_ROOT;
+    /* eslint-enable no-param-reassign */
 
-  next();
-};
+    next();
+  };
