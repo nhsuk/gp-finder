d745f869903a307363ed51ac56f562e7acfbc031
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-07-14 08:42:01 +0100

    :black_nib: Final content changes
    Also, fixed a problem with multiple tests runs and wget erroring when the
    index.html file exists from the previous test run.

--- app/middleware/setLocals.js
+++ app/middleware/setLocals.js
@@ -6,7 +6,7 @@ function fromRequest(req, res, next) {
   res.locals.postcode = req.query.postcode;
   res.locals.isOutcode = false;
   res.locals.searchLabel = 'Surgery or GP name';
-  res.locals.searchButton = 'Next';
+  res.locals.searchButton = 'Search';
 
   const backLink = backLinkUtils(req, res);
 
