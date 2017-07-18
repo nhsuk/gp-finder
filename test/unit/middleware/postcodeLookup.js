803ef3a99cd5624ab44c52a49fbb68dd3d09d23c
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-07-19 00:18:25 +0100

    :stuck_out_tongue: Use new outcode lookup in postcodeio client

--- test/unit/middleware/postcodeLookup.js
+++ test/unit/middleware/postcodeLookup.js
@@ -31,30 +31,5 @@ describe('Postcode lookup', () => {
         .to
         .equal(true, 'Should have passed error to renderer');
     });
-    it('should render an error page for outcode search when postcode.io is not available', () => {
-      const postcodeLookup = rewire('../../../app/middleware/postcodeLookup');
-      const postcodesIOClientFake = {
-        outcode: (postcode, callback) => { callback('Error!'); }
-      };
-
-      const postcodeErrorSpy = sinon.spy();
-      const rendererMock = {
-        postcodeError: postcodeErrorSpy
-      };
-
-      // eslint-disable-next-line no-underscore-dangle
-      postcodeLookup.__set__('PostcodesIO', postcodesIOClientFake);
-      // eslint-disable-next-line no-underscore-dangle
-      postcodeLookup.__set__('renderer', rendererMock);
-
-      postcodeLookup({}, { locals: { postcode: 'HG5' } }, () => {});
-
-      expect(postcodeErrorSpy.calledOnce)
-        .to
-        .equal(true, 'Should have called postcodeError renderer once');
-      expect(postcodeErrorSpy.calledWith('Error!'))
-        .to
-        .equal(true, 'Should have passed error to renderer');
-    });
   });
 });
