64af8dae9e2227b4bc5cbddf05a4c33c37603908
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-08-02 17:33:12 +0100

    :pushpin: Allow a specific branch/tag/commit of the CI scripts to be used

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-git clone --depth 1 --single-branch --branch master https://github.com/nhsuk/ci-deployment.git scripts/ci-deployment
+git clone https://github.com/nhsuk/ci-deployment.git scripts/ci-deployment
+
+if [ -n "${CI_SCRIPT_BRANCH}" ]; then
+  (
+    cd scripts/ci-deployment &&
+    git checkout "${CI_SCRIPT_BRANCH}"
+  )
+fi
 
 bash scripts/ci-deployment/deploy.sh
