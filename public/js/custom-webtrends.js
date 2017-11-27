d75e2c7de2ef1db04dd6f123f7debc65404a946d
mark <markharrop@yahoo.com>
2017-11-27 14:22:24 +0000

    :bug: remove global.webtrends local variable as not initialised on start

--- public/js/custom-webtrends.js
+++ public/js/custom-webtrends.js
@@ -2,14 +2,13 @@
   "use strict"
   var $ = global.jQuery;
   var document = global.document;
-  var WT = global.Webtrends;
 
   $(document).ready(function () {
     $('.details__summary').on('touchstart click', function () {
-      WT.multiTrack({argsa: ['DCSext.GPAppointment', 'Not_Registered', 'WT.dl', '121']});
+      global.Webtrends.multiTrack({argsa: ['DCSext.GPAppointment', 'Not_Registered', 'WT.dl', '121']});
     });
     $('.results__item h3 a').on('touchstart click', function () {
-      WT.multiTrack({argsa: ['DCSext.GPAppClick', $(this).text(), 'DCSext.GPAppRank', $(this).parents(".results__item").data('index'), 'WT.dl', '121']});
+      global.Webtrends.multiTrack({argsa: ['DCSext.GPAppClick', $(this).text(), 'DCSext.GPAppRank', $(this).parents(".results__item").data('index'), 'WT.dl', '121']});
     });
   });
 })(window);
