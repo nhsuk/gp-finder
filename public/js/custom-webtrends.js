5ce3d4fbf6ce4f939f914d14d486bb6db6d5a298
mark <markharrop@yahoo.com>
2017-11-27 15:17:37 +0000

    :droplet: prevent error if webtrends not loaded

--- public/js/custom-webtrends.js
+++ public/js/custom-webtrends.js
@@ -1,14 +1,18 @@
-(function(global){
+(function (global) {
   "use strict"
   var $ = global.jQuery;
   var document = global.document;
 
   $(document).ready(function () {
     $('.details__summary').on('touchstart click', function () {
-      global.Webtrends.multiTrack({argsa: ['DCSext.GPAppointment', 'Not_Registered', 'WT.dl', '121']});
+      if (global.Webtrends) {
+        global.Webtrends.multiTrack({ argsa: ['DCSext.GPAppointment', 'Not_Registered', 'WT.dl', '121'] });
+      }
     });
     $('.results__item h3 a').on('touchstart click', function () {
-      global.Webtrends.multiTrack({argsa: ['DCSext.GPAppClick', $(this).text(), 'DCSext.GPAppRank', $(this).parents(".results__item").data('index'), 'WT.dl', '121']});
+      if (global.Webtrends) {
+        global.Webtrends.multiTrack({ argsa: ['DCSext.GPAppClick', $(this).text(), 'DCSext.GPAppRank', $(this).parents(".results__item").data('index'), 'WT.dl', '121'] });
+      }
     });
   });
 })(window);
